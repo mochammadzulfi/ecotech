dd96ee3660c205e9d39c27c211bbf6253fcfbdca
zulfi <mochammadzulfi@gmail.com>
2026-04-10 05:32:59 +0700

    Update auth.blade.php

--- resources/views/admin/layouts/auth.blade.php
+++ resources/views/admin/layouts/auth.blade.php
@@ -8,9 +8,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
+
+    <style>
+        body,
+        html {
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+    </style>
 </head>
 
-<body class="bg-light d-flex align-items-center justify-content-center min-vh-100">
+<body>
 
     @yield('content')
 
